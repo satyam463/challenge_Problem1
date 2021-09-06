615790d870c7a4e7ceed42468ceed36c60aeb7b0
satyam463 <70562370+satyam463@users.noreply.github.com>
2021-09-06 19:36:42 +0530

    Create challengeproblem.py

---
+++ challengeproblem.py
@@ -0,0 +1,35 @@
+a=6
+b=3
+A=np.array([a+b,a-b])
+print(A)
+B=np.array([a-b,a+b])
+print(B)
+P=(1/(a+b))*(np.column_stack((B,A))@np.array([a,b]).T)
+print(P)
+Q=(1/(a-b))*(np.column_stack((B,A))@np.array([a,-b]).T)
+print(Q)
+x_AB = line_gen(A,B)
+x_AP=line_gen(A,P)
+x_PB=line_gen(P,B)
+x_QB=line_gen(Q,B)
+#x_QA = line_gen(Q,A)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_AP[0,:],x_AP[1,:],label='$AP$')
+plt.plot(x_PB[0,:],x_PB[1,:],label='$PB$')
+plt.plot(x_QB[0,:],x_QB[1,:],label='$PB$')
+
+plt.plot(A[0], A[1], 'o')
+plt.text(A[0] * (1 - 0.01), A[1] * (1 - 0.1) , 'A')
+plt.plot(P[0], P[1], 'o')
+plt.text(P[0] * (1 - 0.01), P[1] * (1 - 0.1) , 'P')
+plt.plot(Q[0], Q[1], 'o')
+plt.text(Q[0] * (1 - 0.01), Q[1] * (1 - 0.1) , 'Q')
+plt.plot(B[0], B[1], 'o')
+plt.text(B[0] * (1 - 0.1), B[1] * (1) , 'B')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.show()
